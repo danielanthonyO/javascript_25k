8d62df13a704cc95ec458c1d70ca0b262c9d1b16
daniel.anthony@edu.bc.fi <daniel.anthony@edu.bc.fi>
2025-01-27 20:59:56 +0200

    Add solutions to variable task

--- 01_week/01_exercises/variables/variables_03.js
+++ 01_week/01_exercises/variables/variables_03.js
@@ -4,3 +4,5 @@ Assign the value "Cat" to animalType and "Whiskers" to animalName.
 Create a new variable named description and combine animalType and animalName into a sentence (e.g., "The Cat is named Whiskers.").
 Use console.log() to print the value of description. 
 */
+
+
