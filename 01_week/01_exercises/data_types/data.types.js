3cf4a973ac3016fda95e4bf91f092b15ad84c3c3
daniel.anthony@edu.bc.fi <daniel.anthony@edu.bc.fi>
2025-01-27 19:31:15 +0200

    Add Javascript folders and exercise on operators

---
+++ 01_week/01_exercises/data_types/data.types.js
@@ -0,0 +1,35 @@
+/*
+1. Create a variable called age and assign it a number (e.g., 25).
+2. Create a variable called name and assign it a string (e.g., "John").
+3. Create a variable called isStudent and assign it a boolean value (true or false).
+4. Create a variable called hobbies and assign it an array with a few strings (e.g., ["reading", "coding", "running"]).
+5. Create an object called info with two properties:
+    schoolName (a string, e.g., "Business College Helsinki")
+    groupName (a number, e.g., REACT25K)
+6. Use console.log() to print each variable to the console.
+7. Use the typeof operator to check and print the data type of each variable.
+*/
+
+// 1. create a variable called `age` and assign it a number (e.g. 25).
+let age = 25;
+
+// 2. create a variable called `name` and assign it a string (e.g. "John").
+const name = "John";
+
+// 3. create a variable called `isStudent` and assign it a boolean (e.g. true or false).
+const isStudent = true;
+
+// 4. create a variable called `hobbies` and assign it an array with few strings strings.
+let hobbies = ["reading", "swimming", "cooking"];
+
+// 5. create an object called `info` with two properties: schoolName (a string, e.g.,"Business College Helsinki") and groupName (a number, e.g. REACT25K).
+let info = {
+  schoolName: "Business College Helsinki",
+  groupName: "REACT25K",
+};
+// use console.log() to print out each variables.
+console.log(age);
+console.log(name);
+console.log(isStudent);
+console.log(hobbies);
+console.log(info);
---
+++ 01_week/01_exercises/operators/operators_01.js
@@ -0,0 +1,50 @@
+/* 
+1. Create two variables, a and b, and assign them integer values (e.g., 8 and 3).
+2. Perform the following operations using a and b, and store the results in separate variables:
+ - Add the values (sum).
+ - Subtract b from a (difference).
+ - Multiply the values (product).
+ - Divide a by b (quotient).
+ - Find the remainder when a is divided by b (remainder).
+3. Use console.log() to print the results of each operation. For example, print: "The sum of a and b is: 11"
+4. Add comments to explain what each operation does.
+5. Change the values of a and b and observe how the results change.
+6. Try the same operations with one positive and one negative number.
+*/
+
+let a = 10;
+let b = 5;
+
+// - Add a and b (sum).
+let sum = a + b;
+console.log("Sum of a and b is:", sum);
+
+// - Subtract b from a (difference).
+let subtract = a - b;
+console.log("Difference of a and be is:", subtract);
+
+// - Multiply a and b (product).
+let product = a * b;
+console.log("Product of a and b is:", product);
+
+// - Divide a by b (quotient).
+let quotient = a / b;
+console.log("Quotient of a and b is:", quotient);
+
+let remainder = a % b;
+console.log("Remainder of a and b is:", remainder);
+
+a = 20;
+b = -10;
+
+// - Add a and b (sum).
+let sum2 = a + b;
+console.log("Sum of a and b is:", sum2);
+
+// - Subtract b from a (difference).
+let subtract2 = a - b;
+console.log("Difference of a and be is:", subtract2);
+
+// - Multiply a and b (product).
+let product2 = a * b;
+console.log("Product of a and b is:", product2);
