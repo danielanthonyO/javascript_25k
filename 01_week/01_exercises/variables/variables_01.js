25ea4d44dd6952967af7fea0e186e1594c073876
daniel.anthony@edu.bc.fi <daniel.anthony@edu.bc.fi>
2025-01-27 19:38:30 +0200

    Add some Javascript exercises on variables

---
+++ 01_week/01_exercises/variables/variables_01.js
@@ -0,0 +1,16 @@
+// create a variable called count and set it to 0
+// on the next line, add 1 to the count variable.
+// print out the value of the count variable.
+
+let count = 0;
+count = count + 1;
+console.log(count);
+
+// count += 1;
+// count++;
+
+/* 
+Create a variable named count and set its value to 0.
+On the next line, add 1 to count.
+Use console.log() to print the value of count. 
+*/
