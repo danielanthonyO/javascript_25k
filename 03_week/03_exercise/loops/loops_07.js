31ff1430211a6fe0d7de51d39073333e11f21fbf
daniel.anthony@edu.bc.fi <daniel.anthony@edu.bc.fi>
2025-02-02 17:02:04 +0200

    Add solutions to some Javascript loop tasks

--- 03_week/03_exercise/loops/loops_07.js
+++ 03_week/03_exercise/loops/loops_07.js
@@ -7,3 +7,28 @@ Write a function named sumAndAverage:
     - The smallest and largest numbers.
 Display all results in the console.
 */
+
+function sumAndAverage() {
+    let sum = 0;
+    let average = 0;
+    let smallest = Infinity;
+    let largest = -Infinity;
+    for (let i = 0; i < 10; i++) {
+        let number = parseInt(prompt('Enter a number: '));
+        console.log(`Number: ${number}`);
+        sum += number;
+        if (number < smallest) {
+            smallest = number;
+        }
+        if (number > largest) {
+            largest = number;
+        }
+    }
+    average = sum / 10;
+    console.log(`Sum: ${sum}`);
+    console.log(`Average: ${average}`);
+    console.log(`Smallest: ${smallest}`);
+    console.log(`Largest: ${largest}`);
+}
+
+sumAndAverage();
