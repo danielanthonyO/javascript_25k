4f060db1cee00752190df5521816d2a0c2347ee4
daniel.anthony@edu.bc.fi <daniel.anthony@edu.bc.fi>
2025-02-02 17:25:15 +0200

    Add solutions to js operator task 2 and 3

--- 01_week/01_exercises/operators/operators_04.js
+++ 01_week/01_exercises/operators/operators_04.js
@@ -5,3 +5,10 @@ Create a new variable named description and combine the numbers into a sentence,
 "The person is 25 years old and 175 cm tall."
 Use console.log() to print the value of description.
 */
+
+// Solution
+let age = 25;
+let height = 165;
+
+let description = age + height;
+console.log(`The person is ${age} years old and ${height} cm tall.`)
