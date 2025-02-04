f88ef037284b44caf583523ee9de87613fbb4f9a
daniel.anthony@edu.bc.fi <daniel.anthony@edu.bc.fi>
2025-02-04 13:11:40 +0200

    Add solutions to Javascript DOM tasks

--- 04_week/04_exercise/basic_DOM/basic_DOM_2/script.js
+++ 04_week/04_exercise/basic_DOM/basic_DOM_2/script.js
@@ -6,3 +6,24 @@ Write JavaScript to:
 - Append the <li> element to the list.
 - Clear the input field after adding the fruit.
 */
+
+// selectors
+const userInput = document.querySelector('#fruitInput');
+const button = document.querySelector('#addFruitBtn');
+const lists = document.getElementById('fruitList');
+
+
+// functions
+function updateFruitList() {
+    // const parent = document
+    const newItem = document.createElement('li');
+    // get content in
+    newItem.textContent = userInput.value;
+    // add to parent
+    lists.appendChild(newItem);
+    // to clear value
+    userInput.value = '';
+}
+
+// trigger
+button.addEventListener('click', updateFruitList);
--- 04_week_practice/practice2/main.js
+++ 04_week_practice/practice2/main.js
@@ -47,13 +47,15 @@
 
 // today <!-- another task1 -->
 // selector
-const button = document.querySelector('.insertBtn'); // class
-const example2 = document.querySelector('#myID')     // ID
-const example3 = document.querySelector('div');  // always specific
+// const button = document.querySelector('.insertBtn'); // class
+// const example2 = document.querySelector('#myID')     // ID
+// const example3 = document.querySelector('div');  // always specific
+// const example4 = document.querySelector('div p');
+// const example5 = document.querySelector('div');
 
-const mybutton = document.getElementById("insertBtn");
-const input = document.getElementById("input");
-const text = document.getElementById("result");
+// const mybutton = document.getElementById("insertBtn");
+// const input = document.getElementById("input");
+// const text = document.getElementById("result");
 
 // function
 // const handleClick = () => {
@@ -64,14 +66,39 @@ const text = document.getElementById("result");
 // mybutton.addEventListener("click", handleClick);
 
 // another task, input from text field
-function displayOutput() {
-    text.textContent = `This is new: ${input.value}`;
+// function displayOutput() {
+//     text.textContent = `This is new: ${input.value}`;
 
-    // text.textContent = input.value;
+//     // text.textContent = input.value;
 
-  // correct version
-  // document.getElementById('result').textContent = document.getElementById('input').value;
-} 
+//   // correct version
+//   // document.getElementById('result').textContent = document.getElementById('input').value;
+// } 
 
-// we can have multiple listeners for many functions
-mybutton.addEventListener("click", displayOutput);
+// // we can have multiple listeners for many functions
+// mybutton.addEventListener("click", displayOutput);
+
+
+
+
+
+// another task, this shows only one of them
+// const textExample1 = document.querySelector('p');
+// //const textExample2 = document.querySelector('.myclass');
+// const textExample3 = document.querySelector('#myid');
+
+
+
+// console.log(textExample1);
+// console.log(textExample2);
+// console.log(textExample3);
+
+// for all
+const textExample4 = document.querySelectorAll('.myclass');
+console.log(textExample4);
+console.log(textExample4[2].textContent);
+console.log(textExample4.textContent);
+
+const textExample5 = document.getElementsByClassName('.myclass');
+console.log(textExample5);
+console.log(textExample5.textContent);
