38c6a8d01c24bdff9c80cb72bcb7c0957e5533c5
daniel.anthony@edu.bc.fi <daniel.anthony@edu.bc.fi>
2025-02-23 01:19:18 +0200

    Modify week 4 task

--- 04_week/04_exercise/pancake_maker_1/script.js
+++ 04_week/04_exercise/pancake_maker_1/script.js
@@ -1,34 +1,56 @@
+// type, only one select
+const pancakeType = document.querySelector('#type');
+console.log(pancakeType); // result is : <select id="type">...</select>
+const toppings = document.querySelectorAll('.topping'); // used class name for all
+console.log(toppings);
+const extras = document.querySelectorAll('.extra'); // used class name for all
+console.log(extras); // result is : NodeList(3) [div.extra, div.extra, div.extra]
+
+const totalPriceDisplay = document.querySelector('#totalPriceDisplay');
+const totalPriceBanner = document.querySelector('#totalPrice');
+console.log(totalPriceDisplay); // result is : <span id="totalPriceDisplay">...</span>
+
+// add event listener to the pancakeType
+// pancakeType.addEventListener('change', function() {
+//   console.log(pancakeType.value); // result is : pancake
+// });
 
 
-const totalPriceElement = document.getElementById('totalPriceDisplay');
-console.log(totalPriceElement);
-const checkboxes = document.querySelectorAll('.topping');
-const totalP = document.getElementById('totalPrice');
-const pancake = document.getElementById('type');
+// function first thing to do is to calculate the total
+const calculateTotal = () => {
+    //let totalPrice = 5; // because 5 is minimum price, parsefloat because of €
+    let totalPrice = parseFloat(pancakeType.value);
+    console.log(totalPrice); // result is : pancake
 
-let totalPrice = 5;
-
-// if(pancake.value = 5){
-//     totalPrice.textContent = totalPrice;
-// }
-// else if(pancake.value = 6){
-//     totalPrice.textContent = totalPrice;
-// }
-// else{
-//     totalPrice.textContent = totalPrice;
-// }
-
-checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', function () {
-        if (this.checked) {
-            totalPrice += 1;
-        } else {
-            totalPrice -= 1;
+    // loop through the toppings
+    toppings.forEach(topping => {
+        if (topping.checked) {
+            totalPrice += parseFloat(topping.value);
         }
-
-        // Update the total price
-        totalPriceElement.textContent = totalPrice;
-        let overallPrice = `${totalPrice} + 5€`
-        console.log(overallPrice);
     });
+
+    // loop through the extras
+    extras.forEach(extra => {
+        if (extra.checked) {
+            totalPrice += parseFloat(extra.value);
+        }
+    });
+
+    // bring in total prices
+    totalPriceDisplay.textContent = `${totalPrice} €`;
+    totalPriceBanner.textContent = `${totalPrice} €`;
+
+}
+
+
+// add event listener to the toppings, when input changes
+pancakeType.addEventListener('change', calculateTotal);
+
+// add event listener to the toppings, when input changes
+toppings.forEach(topping => {
+    topping.addEventListener('change', calculateTotal);
+});
+// add event listener to the extras, when input changes
+extras.forEach(extra => {
+    extra.addEventListener('change', calculateTotal);
 });
