27971b91a32c0746d96a384579ec8eedaf156d1e
daniel.anthony@edu.bc.fi <daniel.anthony@edu.bc.fi>
2025-03-11 14:50:37 +0200

    Modify files and folder structure for week 7

--- 07_week/07_exercise/example.js
+++ 07_week/07_exercise/example.js
@@ -1,12 +1,11 @@
 const classroom = [
-    {name: 'Max', age: 25, grade: 4},
-    {name: 'Brandy', age: 25, grade: 4},
-    {name: 'Beyonce', age: 25, grade: 4}
+  { name: "Max", age: 25, grade: 4 },
+  { name: "Brandy", age: 25, grade: 4 },
+  { name: "Beyonce", age: 25, grade: 4 },
 ];
 
-const jsonClassroom = JSON.stringify(classroom); 
-console.log(jsonClassroom)
-
+const jsonClassroom = JSON.stringify(classroom);
+console.log(jsonClassroom);
 
 /* Task 4 ***********************
 Take the JSON string from Task 3, convert it back into a JavaScript array,
@@ -16,12 +15,11 @@ and log the names of all students in the classroom. */
 const arrayClassroom = JSON.parse(jsonClassroom);
 console.log(arrayClassroom);
 
-arrayClassroom.forEach(student => console.log(student.name));
+arrayClassroom.forEach((student) => console.log(student.name));
 
-// date object to create ID
+// date object to create a unique user id
 const userId = Date.now();
-console.log(userId)
+console.log(userId);
 
-const date = new Date;
-console.log(date)
-
+const date = new Date();
+console.log(date);
